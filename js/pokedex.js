e7cee0de1c58d3dfda5706c491a10cfb95e01265
Mina <minavu@Minas-MacBook-Pro.local>
2021-11-24 23:45:43 -0800

    work on desktop view progressing slowly but surely

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,3 +1,49 @@
+const types_list = [
+	'normal',
+	'fighting',
+	'flying',
+	'poison',
+	'ground',
+	'rock',
+	'bug',
+    'ghost',
+    'steel',
+	'fire',
+	'water',
+	'grass',
+	'electric',
+	'psychic',
+    'ice',
+	'dragon',
+    'dark',
+	'fairy',
+    'unknown',
+    'shadow'
+];
+
+const colors_list = [
+	'#F5F5F5',
+	'#E6E0D4',
+	'#F5F5F5',
+	'#D9D2E9',
+	'#f4e7da',
+	'#d5d5d4',
+	'#f8d5a3',
+    '#d5a6bd',
+    '#e7e7e7',
+	'#faafaf',
+	'#DEF3FD',
+	'#DEFDE0',
+	'#FCF7DE',
+	'#eaeda1',
+    '#e0eefb',
+	'#97b3e6',
+    '#76a5af',
+	'#fceaff',
+    '#5b5b5b',
+    '#ea9999'
+];
+
 
 let screen = document.querySelector(".pokedex-screen");
 let back = document.getElementById("back");
@@ -11,6 +57,19 @@ let addPokemonCard = (pokemon) => {
     let name = document.createElement("h3");
     let image = document.createElement("img");
     card.setAttribute("class", "pokemon-card");
+
+    let {types: [type1, type2]} = pokemon;
+    let {type: {name: type1_name}} = type1;
+
+    let index1 = types_list.indexOf(`${type1_name}`);
+    card.style.backgroundColor = colors_list[index1];
+    
+    if (type2) {
+        let {type: {name: type2_name}} = type2;
+        let index2 = types_list.indexOf(`${type2_name}`);
+        card.style.background = `linear-gradient(${colors_list[index1]}, ${colors_list[index2]})`;
+    }
+
     name.textContent = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
     image.setAttribute("class", "pokemon-image");
     image.setAttribute("src", pokemon.sprites.front_default);
@@ -23,7 +82,6 @@ let addPokemonCard = (pokemon) => {
 let loadPage = (start, end) => {
     document.querySelectorAll(".pokemon-card").forEach(card => card.remove());
     for (let id = start + 1; id <= end; id++) {
-        console.log(id, end);
         let pokeapi = `https://pokeapi.co/api/v2/pokemon/${id}`;
         let fetchData = async (pokeapi) => {
             try {
@@ -38,14 +96,18 @@ let loadPage = (start, end) => {
     }
 }
 
-
 loadPage(start, end);
 
-// back.onclick = () => {
-    //     loadPage(start-=10, limit);
-    // }
+back.onclick = () => {
+    if (start === 0) return;
+    start = start - limit;
+    end = start + limit;
+    loadPage(start, end);
+}
     
 next.onclick = () => {
-    limit = (start + limit >= 898) ? 898 : limit + start;
-    loadPage(start+=10, limit);
+    if (end === 898) return;
+    start = start + limit;
+    end = (start + limit >= 898) ? 898 : start + limit;
+    loadPage(start, end);
 }
