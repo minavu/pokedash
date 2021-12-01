c0a97abec8d04df809496cff04164dc54c422957
rachana <bolla.rachana@gmail.com>
2021-12-01 02:23:38 -0800

    dashboard completed nav bar errors fixed

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,95 +1,71 @@
-const types_list = [
-	'normal',
-	'fighting',
-	'flying',
-	'poison',
-	'ground',
-	'rock',
-	'bug',
-    'ghost',
-    'steel',
-	'fire',
-	'water',
-	'grass',
-	'electric',
-	'psychic',
-    'ice',
-	'dragon',
-    'dark',
-	'fairy',
-    'unknown',
-    'shadow'
+/* --------- Data has taken from the Pokemon Wiki -----------------  */
+/*---------- Bar Graph to display Pokemons by Region ---------------------*/
+
+var xValues = ["Kanto", "Johto", "Hoenn", "Sinnoh", "Unova", "Kalos", "Alola", "Galar"];
+
+var yValues = [151, 100, 135, 107, 156, 72, 88, 89];
+var barColors = ["red", "green","blue","orange","brown","purple","cyan","navy"];
+
+new Chart("pokeChart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: false,
+      text: "Pokemon's by Region"
+    }
+  }
+});
+
+/*---------- Donut chart to display Pokemon by Types ---------------------*/
+
+var xValues1 = ["Bug","Dark","Dragon","Electric","Fairy","Fighting","Fire","Flying","Ghost","Grass","Ground","Ice","Normal","Poison","Psychic","Rock","steel","Water"];
+var yValues1 = [61, 52, 35, 44, 34, 46, 55, 88, 34, 83, 65, 38, 102, 64, 82, 52, 48, 25];
+var barColors1 = [
+  "#5F9EA0",
+  "#6495ED",
+  "#800080",
+  "#DFFF00",
+  "#FF00FF",
+  "#DE3163",
+  "#FF5F1F",
+  "#FFBF00",
+  "#AAFF00",
+  "#00FFFF",
+  "#F5DEB3",
+  "#808000",
+  "#F0E68C",
+  "#800020",
+  "#CD7F32",
+  "#0437F2",
+  "#CCCCFF",
+  "#00008B"
 ];
 
-const colors_list = [
-	'#F5F5F5',
-	'#E6E0D4',
-	'#F5F5F5',
-	'#D9D2E9',
-	'#f4e7da',
-	'#d5d5d4',
-	'#f8d5a3',
-    '#d5a6bd',
-    '#e7e7e7',
-	'#faafaf',
-	'#DEF3FD',
-	'#DEFDE0',
-	'#FCF7DE',
-	'#eaeda1',
-    '#e0eefb',
-	'#97b3e6',
-    '#76a5af',
-	'#fceaff',
-    '#5b5b5b',
-    '#ea9999'
-];
-
-let screen = document.querySelector(".bar-Chart");
-let modal_screen = document.querySelector(".bar-Chart1");
-
-
-let addPokemonCard = (pokemon) => {
-    let card = document.createElement("div");
-    let name = document.createElement("h3");
-    let image = document.createElement("img");
-    
-    let {types: [type1, type2]} = pokemon;
-    let {type: {name: type1_name}} = type1;
-    let index1 = types_list.indexOf(`${type1_name}`);
-	
-	//console.log(":"+type1.type.name+":");
-	  
-	
-   /* if (type2) {
-        let {type: {name: type2_name}} = type2;
-        let index2 = types_list.indexOf(`${type2_name}`);
-        card.style.background = `linear-gradient(${colors_list[index1]}, ${colors_list[index2]})`;
-    } else {
-        card.style.backgroundColor = colors_list[index1];
+new Chart("myChart", {
+  type: "doughnut",
+  data: {
+    labels: xValues1,
+    datasets: [{
+      backgroundColor: barColors1,
+      data: yValues1
+    }]
+  },
+  options: {
+    legend: {
+        display: false, 
+        position: "left"
+       },
+    title: {
+      display: false,
+      text: "Pokemon Types"
     }
-    */
-	
-
-}
-
-
-
-let loadPage = () => {
-    document.querySelectorAll(".pokemon-card").forEach(card => card.remove());
-    for (let id = 1; id <= 898; id++) {
-        let pokeapi = `https://pokeapi.co/api/v2/pokemon/${id}`;
-        let fetchData = async (pokeapi) => {
-            try {
-                let response = await fetch(pokeapi);
-                let pokemon = await response.json();
-			//	console.log(id+ " : " +pokemon.types_list);
-               addPokemonCard(pokemon);
-            } catch(error) {
-               // console.error(error);
-            }
-        }
-        fetchData(pokeapi);
-    }
-}
-
-loadPage();
+  }
+});
