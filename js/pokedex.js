20b6a1c49d75b1f5a8081d805e0ba511a39b4d82
Mina <minavu@Minas-MacBook-Pro.local>
2021-12-01 23:23:02 -0800

    code comments for the pokedex html, css, js pages done

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,3 +1,4 @@
+// This is an array containing all the types of pokemon.
 const types_list = [
 	'normal',
 	'fighting',
@@ -21,6 +22,7 @@ const types_list = [
     'shadow'
 ];
 
+// This is an array containing the colors to represent the types of pokemon in the corresponding array above.
 const colors_list = [
 	'#F5F5F5',
 	'#E6E0D4',
@@ -44,6 +46,8 @@ const colors_list = [
     '#ea9999'
 ];
 
+// This block of code queries different selectors on the pokedex page for later processing.  There are also
+// variables created to limit the number of pokemons to fetch at one time.
 let screen = document.querySelector(".pokedex-screen");
 let modal_screen = document.querySelector(".modal-screen");
 let close = document.querySelector(".close");
@@ -53,11 +57,16 @@ let start = 0;
 let limit = 12;
 let end = start + limit;
 
+// This is the onclick function for the close button in the modal screen.  When clicked, the display will disappear.
 close.onclick = () => {
     modal_screen.style.display = "none";
     screen.style.overflow = "auto";
 }
 
+// This is a function to add the pokemon name and type to the modal screen.  It takes a pokemon parameter from
+// the api fetch with pokemon information.  It creates a card container div, a name element h3, and 3 h5 tags
+// for the types.  The type section displays the title "TYPES" underlined and either 1 or 2 types depending on 
+// the pokemon.  This card gets appended to the modal screen with the class name "pokemon-modal".
 let modalName = (pokemon) => {
     let card = document.createElement("div");
     let name = document.createElement("h3");
@@ -86,6 +95,11 @@ let modalName = (pokemon) => {
     modal_screen.append(card);
 }
 
+// This is a function of add pokemon images to the modal screen and takes the pokemon parameter gathered from an
+// api call.  First, it creates two card container divs and 4 image elements.  Then, it sets the attributes for all
+// images giving them class name of "pokemon-modal-image" in order to be styled and alt text.  It appends the regular
+// front and back images to card 1 and appends the shiny front and back images to card 2 so that all images don't
+// appear on the same line.  Both cards are classed as "pokemon-modal" and then appended to the modal screen.
 let modalImages = (pokemon) => {
     let card1 = document.createElement("div");
     let card2 = document.createElement("div");
@@ -116,20 +130,17 @@ let modalImages = (pokemon) => {
     modal_screen.append(card2);
 }
 
+// This is the function to add a pokemon stat chart to the modal screen and takes the pokemon json document from 
+// an api fetch call.  First, it capitalizes the pokemon name.  Then, it destructures the stats field from the 
+// json document and further destructures the array containing all 6 stat properties.  It creates a card to contain
+// information and a canvas with a transparent background to contain the chart.  A new radar chart is created 
+// with scale from 0 to 100 stepping by 20.  Correct labels and stat data are added.  The card gets appended to the
+// modal screen with class name "pokedex-modal".
 let modalStats = (pokemon) => {
     let name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
     let {stats} = pokemon;
     let [hp, attack, defense, specialAttack, specialDefense, speed] = stats;
 
-    let test = () => {
-        console.log(hp.stat.name, hp.base_stat);
-        console.log(attack.stat.name, attack.base_stat);
-        console.log(defense.stat.name, defense.base_stat);
-        console.log(specialAttack.stat.name, specialAttack.base_stat);
-        console.log(specialDefense.stat.name, specialDefense.base_stat);
-        console.log(speed.stat.name, speed.base_stat);
-    }
-    // test();
     let card = document.createElement("div");
     let canvas = document.createElement("canvas");
     canvas.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
@@ -172,6 +183,14 @@ let modalStats = (pokemon) => {
     modal_screen.append(card);
 }
 
+// This is a function to display all information in the modal screen.  It takes a pokemon parameter to process and
+// pass to the functions that it will call.  The first thing it has to do is the query all the elements with class
+// "pokemon-modal" so that it can remove it from the screen.  It also removes the background property of the modal
+// in case a linear gradient was set previously.  This is important in order to display a solid color background if
+// that is needed in the next display of the modal screen.  The center section of this function determines the new
+// background for the modal depending on the pokemon it was given as a parameter.  Pokemons with 2 types will get
+// a linear gradient background and 1 type will get a solid color background.  It calls the 3 functions to display 
+// the name and types, the images, and the stat chart.  Lastly, it changes the display of the modal screen to block.
 let displayInfoModal = (pokemon) => {
     document.querySelectorAll(".pokemon-modal").forEach(card => card.remove());
     modal_screen.style.removeProperty("background");
@@ -194,6 +213,11 @@ let displayInfoModal = (pokemon) => {
     modal_screen.style.display = "block";
 }
 
+// This is the function to add a single pokemon card to the pokedex screen.  It takes a pokemon fetched.  It creates
+// a card container div, a name element h3, and an image element.  It checks for the types of the pokemon and uses
+// the constants type array and color array above to color the background of the card.  It capitalizes the name and
+// sets attributes to the image.  It appends the card to the screen with a give class name for styling.  An onclick
+// event is added to call the display modal function above.
 let addPokemonCard = (pokemon) => {
     let card = document.createElement("div");
     let name = document.createElement("h3");
@@ -224,6 +248,9 @@ let addPokemonCard = (pokemon) => {
     }
 }
 
+// This is the function to fetch pokemons.  It takes the start and end numbers of the id of pokemon to add to the 
+// url to fetch.  The start and end parameters determine how many pokemons will be called.  However, it must first
+// clear the screen by removing any existing pokemon card.  It uses a for loop to fetch pokemons from the PokeAPI.
 let loadPage = (start, end) => {
     document.querySelectorAll(".pokemon-card").forEach(card => card.remove());
     for (let id = start + 1; id <= end; id++) {
@@ -241,15 +268,19 @@ let loadPage = (start, end) => {
     }
 }
 
+// Here we call the load function when the pokedex page is loaded.
 loadPage(start, end);
 
+// This is the function to load the previous set of pokemons.  Nothing will be loaded if starting id is 0.
 back.onclick = () => {
     if (start === 0) return;
     start = start - limit;
     end = start + limit;
     loadPage(start, end);
 }
-    
+
+// This is the function to load the next set of pokemons.  When the end is near, only load to the last pokemon
+// to avoid error responses.
 next.onclick = () => {
     if (end === 898) return;
     start = start + limit;
